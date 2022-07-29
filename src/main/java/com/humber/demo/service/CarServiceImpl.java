b334bd7c47128fa2b1c87002a38879a6f9f39685
Tanishq Sharma <tanishqsh@gmail.com>
2022-07-28 22:34:49 -0400

    car service implementation

---
+++ src/main/java/com/humber/demo/service/CarServiceImpl.java
@@ -0,0 +1,18 @@
+package com.humber.demo.service;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.humber.demo.model.Car;
+import com.humber.demo.repository.CarRepository;
+
+public class CarServiceImpl implements CarService {
+	
+	@Autowired
+	private CarRepository carRepository;
+
+	// returns all the cars
+	@Override
+	public List<Car> getAllCars() {
+		return carRepository.findAll();
+	}
+
+}
